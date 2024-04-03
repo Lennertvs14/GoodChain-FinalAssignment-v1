fc2f6dbdb0fbb44893b717a48c834ebd017c5df8
Lennert <112725488+Lennertvs14@users.noreply.github.com>
2024-04-03 17:39:59 +0200

    Added a public menu interface
    When a user starts the application, it'll display the public menu for user navigation.

---
+++ goodchain/src/goodchain.py
@@ -0,0 +1,41 @@
+import os
+import platform
+
+
+def clear_console():
+    """
+    Clears the console screen for Windows, macOS and Linux.
+    """
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+# Start the application with a public menu interface
+print("Goodchain Public Menu\n")
+print(
+    "1 - Login\n"
+    "2 - Explore the blockchain\n"
+    "3 - Sign up\n"
+    "4 - Exit\n"
+)
+
+chosen_menu_item = input("-> ")
+
+try:
+    chosen_menu_item = int(chosen_menu_item)
+    match chosen_menu_item:
+        case 1:
+            clear_console()
+            print("Log in")
+        case 2:
+            clear_console()
+            print("Explore the blockchain")
+        case 3:
+            clear_console()
+            print("Sign up")
+        case _:
+            exit()
+except ValueError:
+    print("Enter digits only.")
