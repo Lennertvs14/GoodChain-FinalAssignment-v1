3dd35dab8ed20f689b6e0c9d1deb2d880f10f13f
Lennert <112725488+Lennertvs14@users.noreply.github.com>
2024-04-09 17:39:11 +0200

    Enhanced security by limiting login attempts
    Implemented a limit on consecutive login attempts. The program will now stop after 6 unsuccessful login attempts, preventing endless login loops.

--- goodchain/src/login.py
+++ goodchain/src/login.py
@@ -10,23 +10,27 @@ import hashlib
 # Functions
 def login():
     """" Returns a node upon a successful login attempt """
-    # Get necessary user input
-    username = input(" Enter a username -> ")
-    password = input(" Enter a password -> ")
+    attempts = 1
+    max_amount_of_attempts = 7
+    while attempts < max_amount_of_attempts:
+        # Get necessary user input
+        username = input(" Enter a username -> ")
+        password = input(" Enter a password -> ")
 
-    # Assert username
-    node_entity = get_node_by_username(username)
-    if node_entity:
-        # Assert passwords
-        entered_password_hash = get_password_hash_value(password)
-        expected_password_hash = node_entity[1]
-        if entered_password_hash == expected_password_hash:
-            # Login attempt successful, now return the corresponding node object
-            return create_node_from_entity(node_entity)
+        # Assert username
+        node_entity = get_node_by_username(username)
+        if node_entity:
+            # Assert passwords
+            entered_password_hash = get_password_hash_value(password)
+            expected_password_hash = node_entity[1]
+            if entered_password_hash == expected_password_hash:
+                # Login attempt successful, now return the corresponding node object
+                return create_node_from_entity(node_entity)
 
-    # Login attempt unsuccessful
-    print("Failed to login, check your input and try again.\n")
-    return login()
+        # Login attempt unsuccessful
+        remaining_attempts = (max_amount_of_attempts - attempts) - 1
+        print(f"Failed to login, you have {remaining_attempts} attempt(s) left.\n")
+        attempts += 1
 
 
 def get_password_hash_value(password):
