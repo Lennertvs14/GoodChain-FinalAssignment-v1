282addf7679bfe322139e18e56851ef1edb71cef
Lennert <112725488+Lennertvs14@users.noreply.github.com>
2024-04-09 14:37:04 +0200

    Created signature functionalities with the cryptograhpy module

---
+++ goodchain/src/signature.py
@@ -0,0 +1,34 @@
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
+
+
+def sign(message, private_key):
+    message = bytes(str(message), 'utf-8')
+    signature = private_key.sign(
+        message,
+        padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH),
+        hashes.SHA256()
+    )
+    return signature
+
+
+def verify(message, signature, pbc_ser):
+    message = bytes(str(message), 'utf-8')
+    public_key = serialization.load_pem_public_key(pbc_ser)
+    try:
+        public_key.verify(
+            signature,
+            message,
+            padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+                        salt_length=padding.PSS.MAX_LENGTH),
+            hashes.SHA256()
+        )
+        return True
+    except InvalidSignature:
+        return False
+    except:
+        print("An error occurred, please try again.")
+        return False
