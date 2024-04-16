8522ff65626a13a4331e3e98438ee33106c446eb
LennertvS <lennertvansplunder@gmail.com>
2024-04-16 09:55:33 +0200

    Created the private node menu interface

--- goodchain/src/goodchain.py
+++ goodchain/src/goodchain.py
@@ -51,6 +51,59 @@ def handle_public_menu_user_input():
         print("Enter digits only.")
 
 
+def show_private_menu():
+    """ Shows the private menu (meant for nodes only) """
+    print("Welcome \n")
+    print(
+        "1 - Profile\n"
+        "2 - Explore the blockchain\n"
+        "3 - Send coins\n"
+        "4 - Explore the transaction pool\n"
+        "5 - Mine\n"
+        "6 - Show transaction history\n"
+        "7 - Exit\n"
+    )
+
+
+def handle_private_menu_user_input():
+    """ Handles node input for the private menu interface """
+    global node
+    chosen_menu_item = input("-> ")
+    try:
+        chosen_menu_item = int(chosen_menu_item)
+        match chosen_menu_item:
+            case 1:
+                clear_console()
+                print("Profile")
+            case 2:
+                clear_console()
+                print("Explore the blockchain")
+            case 3:
+                clear_console()
+                print("Send Coins")
+                node.send_coins()
+            case 4:
+                clear_console()
+                print("Explore the transaction pool")
+                transaction_pool.check_pool()
+            case 5:
+                clear_console()
+                print("Mine")
+                node.mine()
+            case 6:
+                clear_console()
+                print("Transaction history")
+                print(node.wallet.transactions)
+            case 7:
+                clear_console()
+                print("You're logged out, thanks for using GoodChain!")
+                node = None
+            case _:
+                raise ValueError("Invalid choice.")
+    except ValueError:
+        print("Enter valid digits only.")
+
+
 def run_goodchain_app():
     """ Runs the goodchain application """
     global node
@@ -64,7 +117,12 @@ def run_goodchain_app():
         pass
     else:
         clear_console()
-        return run_goodchain_app()
+        # Start the application with a public menu interface
+        show_public_menu()
+        handle_public_menu_user_input()
+
+    clear_console()
+    return run_goodchain_app()
 
 
 if __name__ == "__main__":
