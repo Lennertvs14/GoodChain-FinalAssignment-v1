4fa2b137080cb5194bd861d3c0a03e9a11096482
LennertvS <lennertvansplunder@gmail.com>
2024-04-17 13:33:22 +0200

    Refactored database logic to adhere to object-oriented design principles

--- goodchain/src/database.py
+++ goodchain/src/database.py
@@ -1,16 +1,30 @@
-""" Perform queries on the node database """
-
 import sqlite3
+from typing import List
+from transaction_block import TransactionBlock
 
+path = "../data/ledger.dat"
 
-def connect_to_database(func):
-    def wrapper(*args, **kwargs):
-        # Open connection
-        database = '../data/database.db'
-        connection = sqlite3.connect(database)
+class Database:
+    """ Perform queries on the node database """
+    def __init__(self):
+        self.database = '../data/database.db'
+        self.connection = sqlite3.connect(self.database)
+        self.cursor = self.connection.cursor()
+        self.initialize_database()
+
+    def handle_connection(func):
+        def wrapper(self, *args, **kwargs):
+            # Execute function
+            result = func(self, *args, **kwargs)
+            # Commit changes and close connection
+            self.connection.commit()
+            return result
+        return wrapper
+
+    @handle_connection
+    def initialize_database(self):
         # Ensure Node table exists
-        cursor = connection.cursor()
-        cursor.execute(
+        self.cursor.execute(
             """ CREATE TABLE IF NOT EXISTS Node (
                 Username text,
                 PasswordHash text,
@@ -18,40 +32,29 @@ def connect_to_database(func):
                 PrivateKey text
             )"""
         )
-        connection.commit()
-        # Execute function
-        result = func(connection, cursor, *args, **kwargs)
-        # Close connection
-        connection.close()
-        return result
-    return wrapper
 
-
-@connect_to_database
-def insert_node(connection, cursor, node):
-    """ Inserts a node into the database """
-    node_dict = {
-        'username': node.username,
-        'password_hash': node.password_hash,
-        'public_key': node.public_key,
-        'private_key': node.private_key
-    }
-    with connection:
-        cursor.execute(
+    @handle_connection
+    def insert_node(self, node: TransactionBlock):
+        """ Inserts a node into the database """
+        node_dict = {
+            'username': node.username,
+            'password_hash': node.password_hash,
+            'public_key': node.public_key,
+            'private_key': node.private_key
+        }
+        self.cursor.execute(
             """ INSERT INTO Node 
                 VALUES (:username, :password_hash, :public_key, :private_key)
                 """, node_dict)
 
+    @handle_connection
+    def get_node_by_username(self, username: str):
+        """ Returns a node by username """
+        self.cursor.execute("SELECT * FROM Node WHERE Username = :username", {'username': username})
+        return self.cursor.fetchone()
 
-@connect_to_database
-def get_node_by_username(connection, cursor, username):
-    """ Returns a node by username """
-    cursor.execute("SELECT * FROM Node WHERE Username = :username", {'username': username})
-    return cursor.fetchone()
-
-
-@connect_to_database
-def get_node_username_by_public_key(connection, cursor, public_key):
-    """ Returns a node by username """
-    cursor.execute("SELECT Username FROM Node WHERE PublicKey = :publicKey", {'publicKey': public_key})
-    return cursor.fetchone()[0]
+    @handle_connection
+    def get_node_username_by_public_key(self, public_key: str):
+        """ Returns a node by username """
+        self.cursor.execute("SELECT Username FROM Node WHERE PublicKey = :publicKey", {'publicKey': public_key})
+        return self.cursor.fetchone()[0]
