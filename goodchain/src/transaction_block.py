9ead04568659b7d457261936f7a599b7a975a0ae
Lennert <112725488+Lennertvs14@users.noreply.github.com>
2024-04-21 21:57:47 +0200

    Keep the mining process between 10 to 20 seconds.

--- goodchain/src/transaction_block.py
+++ goodchain/src/transaction_block.py
@@ -25,7 +25,9 @@ class TransactionBlock(Block):
 
     def mine(self, leading_zero):
         print("\nMining..")
-        start = time()
+        start_time = time()
+        end_time = None
+
         digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
         digest.update(bytes(str(self.data), 'utf-8'))
         digest.update(bytes(str(self.previous_block_hash), 'utf-8'))
@@ -38,13 +40,16 @@ class TransactionBlock(Block):
             new_digest.update(bytes(str(nonce), 'utf-8'))
             new_hash = new_digest.finalize()
             if new_hash[:leading_zero] == bytes('0' * leading_zero, 'utf-8'):
-                if int(new_hash[leading_zero]) < timing_variable:
+                if int(new_hash[leading_zero]) < timing_variable and (time() - start_time) > 10:
                     found = True
+                    end_time = time()
                     self.nonce = nonce
+                elif (time() - start_time) > 20:
+                    print("The mining process took too long, please try again later.")
+                    return
             nonce += 1
             del new_digest
 
-        end = time()
-        computation_time = end - start
+        computation_time = end_time - start_time
         self.block_hash = self.compute_hash()
         print(f"Block #{self.id} is mined in {computation_time} seconds.")
